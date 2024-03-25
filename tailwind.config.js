1ac1a982e1096eb484e0ffb086c68c68e362da28
fayyadhaqillahauthar <fayyadhaqillahauthar@gmail.com>
2024-03-25 10:55:04 +0700

    ok

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        f1:"#00a2c6",
+        f2:"#088da4",
+      },
+      backgroundImage:{
+      } 
+    },
+  },
+  plugins: [],
+  darkMode: "class"
+}
+
